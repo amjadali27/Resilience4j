76820ab7cdea3083a13679d894daff8d46d66f7e
Amjad Ali <ali.amjad@systemsltd.com>
2021-09-26 02:47:14 +0500

    Rate Limiter code

---
+++ src/main/java/com/learning/Resilience4jProjectApplication.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jProjectApplication.class, args);
+	}
+
+}
